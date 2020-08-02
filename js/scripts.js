24c2fa5af23700e5703d04c56c2c76148fae221a
Nikita <nniki.note@gmail.com>
2020-08-02 21:32:33 +0300

    learn if + else, switch

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,34 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if (4 == 9) {
+    console.log('ok');
+} else {
+    console.log('erroe');
+}
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+const num = 50;
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+if (num < 49) {
+    console.log('error');
+} else if (num > 100) {
+    console.log('too much');
+} else {
+    console.log('ok');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+(num == 50) ? console.log('ok') : console.log('error');
 
-console.log(personalMovieDB);
-
+switch (num) {
+    case 49:
+        console.log('error');
+        break;
+    case 100:
+        console.log('error');
+        break;
+    case 51:
+        console.log('ok');
+        break;
+    default:
+        console.log('not in this case');
+        break;
+}
