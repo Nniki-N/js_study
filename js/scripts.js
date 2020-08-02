81fb8045628c0e91851be9de46f39be79c941523
Nikita <nniki.note@gmail.com>
2020-08-02 22:48:46 +0300

    learn while, do while, for

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,23 @@
 "use strict";
 
-if (4 == 9) {
-    console.log('ok');
-} else {
-    console.log('erroe');
-}
+let num = 50;
 
-const num = 50;
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
 
-if (num < 49) {
-    console.log('error');
-} else if (num > 100) {
-    console.log('too much');
-} else {
-    console.log('ok');
-}
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
 
-(num == 50) ? console.log('ok') : console.log('error');
+for (let i = 1; i < 10; i++) {
+    if (i === 6){
+        // break;
+        continue;
+    }
 
-switch (num) {
-    case 49:
-        console.log('error');
-        break;
-    case 100:
-        console.log('error');
-        break;
-    case 51:
-        console.log('ok');
-        break;
-    default:
-        console.log('not in this case');
-        break;
-}
+    console.log(i);
+} 
