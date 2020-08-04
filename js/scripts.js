71ac7620d0a87b9318ec252fafdac438b8652eff
Nikita <nniki.note@gmail.com>
2020-08-04 17:29:29 +0300

    learn passing data by reference or by value, spread(es6-es9)

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,93 @@
 "use strict";
 
-const arr = [1, 13, 9, 3, 7, 4];
-arr.sort(compareNum);
-console.log(arr);
+// let a = 5;
+// let b = a;
 
-function compareNum (a, b) {
-    return a - b;
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; // ссылку
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy (mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return(objCopy);
 }
 
-// arr[99] = 0;
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
 
-// console.log(arr.length);
+const newNumbers = copy(numbers);
 
-arr.forEach(function (item, i, arr) {
-    console.log(`${i}: ${item} внутри массива ${arr}`);
-});
+newNumbers.a = 10;
+newNumbers.c.x = 10;
 
-// arr.pop();
-// arr.push(6);
+console.log(numbers);
+console.log(newNumbers);
 
-// console.log(arr);
+const add = {
+    d: 17,
+    e: 20
+};
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+const clone = (Object.assign({}, add));
 
-// for ( let value of arr) {
-//     console.log(value);
-// }
+clone.d = 20;
 
-const str = prompt('', '');
-const products = str.split(', ');
-products.sort();
-console.log(products.join(' - '));
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[2] = 'sddfs';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log (a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
