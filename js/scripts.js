ee390027d12545fc2cc9d45607d8b90136c9fd1e
Nikita <nniki.note@gmail.com>
2020-08-06 19:32:14 +0300

    learn classlist and delegation

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,40 @@
 "use strict";
 
-// console.log(document.querySelector('#current').parentNode.parentNode);
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
-// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// console.log(btns[0].classList.add('fsbsb'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
 
-for ( let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// } else {
+//     console.log('yellow');
+// }
+
+// btns[0].addEventListener('click', (e) => {
+//     // if (!btns[1].classList.contains('blue')) {
+//     //     btns[1].classList.add('blue');
+//     // } else {
+//     //     btns[1].classList.remove('blue');
+//     // }
+//     btns[1].classList.toggle('blue');
+// });
+
+wrapper.addEventListener('click', (e) => {
+    // console.dir(e.target);
+    if (e.target && event.target.tagName == 'BUTTON') {
+        console.log('hello');
     }
-    
-    console.log(node);
-}
+});
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('hello');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('blue');
+wrapper.append(btn);
