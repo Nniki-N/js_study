830c67f4e09207187d8044d31eed37a35636bc39
Nikita <nniki.note@gmail.com>
2020-08-20 22:00:03 +0300

    finish practice with calculate by using AJAX

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,30 @@
 "use strict";
 
-class Rectanbgle {
-    constructor (height, width) {
-        this.height = height;
-        this.width = width;
-    }
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
 
-    calcArea () {
-        return this.height * this.width;
-    }
-}
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
 
-class ColoredRectangleWithText extends Rectanbgle{
-    constructor(height, width, text, bg) {
-        super(height, width);
-        this.text = text;
-        this.bg = bg;
-    }
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'aplication/json; charset=utf-8');
+    request.send();
 
-    showMyProps () {
-        console.log(`Текст: ${this.text}, цвет: ${this.bg}`);
-    }
-}
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd.toFixed(2));
+        } else {
+            inputUsd.value = 'Что-то не так';
+        }
+    });
 
-const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
+    // status
+    // statusText
+    // response
+    // readyState
 
-div.showMyProps();
-console.log(div.calcArea());
 
-// const square = new Rectanbgle(10, 10);
-// const long = new Rectanbgle(20, 10);
+});
 
-// console.log(square.calcArea());
-// console.log(long.calcArea());
